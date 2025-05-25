25a261fcd8c06b06bb53f7ca30a1a32faf50f451
Sullivan Amadike <amadikesullivan20@gmail.com>
2025-05-25 17:23:50 +0100

    added iframe and error correction

--- app/services/event/upcoming/page.tsx
+++ app/services/event/upcoming/page.tsx
@@ -3,12 +3,13 @@ import UpcomingEventCom from "@/components/general/upcoming";
 import UpcomingHeroSection from "@/components/sections/upcoming/hero";
 import { upcomingEventsSccheduled } from "@/data/upcoming";
 import { names } from "@/data/elllipse";
+import UpcomingEventSection from "@/components/sections/upcoming/events";
 
 export default function UpcomingEventsPage() {
     return (
         <>
             <UpcomingHeroSection />
-            <UpcomingEventCom EventsData={upcomingEventsSccheduled} />
+            <UpcomingEventSection />
             <GeneralEllipse name={names.upcoming} />
         </>
     )
